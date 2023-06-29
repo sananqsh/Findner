86f1cb19f6c0058b92898a0b594bd88e385708e8
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-06-29 15:57:56 +0330

    Add jsonification to serializer

--- serializers.py
+++ serializers.py
@@ -1,15 +1,20 @@
 from models import Partner
-from shapely import wkb
+
+import json
 
 class PartnerSerializer:
     def serialize(partner: Partner):
         if partner:
+            # Convert the result to a dictionary and parse 'coverageArea' and 'address' to GeoJSON
+            partner_dict = dict(partner)
+            partner_dict["coverageArea"] = json.loads(partner_dict["coverageArea"])
+            partner_dict["address"] = json.loads(partner_dict["address"])
             return {
-                "id": partner.id,
-                "tradingName": partner.tradingName,
-                "ownerName": partner.ownerName,
-                "document": partner.document,
-                "coverageArea": str(wkb.loads(str(partner.coverageArea), hex=True)),
-                "address": str(wkb.loads(str(partner.address), hex=True))
+                "id": partner_dict["id"],
+                "tradingName": partner_dict["tradingName"],
+                "ownerName": partner_dict["ownerName"],
+                "document": partner_dict["document"],
+                "coverageArea": partner_dict["coverageArea"],
+                "address": partner_dict["address"]
             }
         return {}
