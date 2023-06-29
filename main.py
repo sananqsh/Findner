0cc462b5401725c082c9502693e28fa192d7a4e4
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-06-29 07:50:37 +0330

    Update response_model for current APIs

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/partners/", response_model=schemas.Partner)
+@app.post("/partners/", response_model=schemas.PartnerResponse)
 def create_partner(partner: schemas.PartnerCreate, db: Session = Depends(get_db)):
     db_partner = crud.get_partner_by_document(db, document=partner.document)
     if db_partner:
         raise HTTPException(status_code=400, detail="Document already registered")
     return crud.create_partner(db=db, partner=partner)
 
-@app.get("/partners/", response_model=list[schemas.Partner])
+@app.get("/partners/", response_model=list[schemas.PartnerResponse])
 def read_partners(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
     partners = crud.get_partners(db, skip=skip, limit=limit)
     return partners
