4915ccf232d0d75dc92f2cfecb40bb2073d6515a
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-06-29 04:22:06 +0330

    Add GET and POST paths to "/partners/"

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-import models
-from database import engine
+import crud, models, schemas
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/partners/", response_model=schemas.Partner)
+def create_partner(partner: schemas.PartnerCreate, db: Session = Depends(get_db)):
+    db_partner = crud.get_partner_by_document(db, document=partner.document)
+    if db_partner:
+        raise HTTPException(status_code=400, detail="Document already registered")
+    return crud.create_partner(db=db, partner=partner)
+
+@app.get("/partners/", response_model=list[schemas.Partner])
+def read_partners(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
+    partners = crud.get_partners(db, skip=skip, limit=limit)
+    return partners
