f9179846f09cd038a40530256765bff1d9c20605
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-06-29 15:56:20 +0330

    Fix json response for coverageArea and address

--- crud.py
+++ crud.py
@@ -7,9 +7,20 @@ from sqlalchemy.orm import Session
 import models, schemas
 from serializers import PartnerSerializer
 
+import json
+
 def get_partner(db: Session, partner_id: str):
-    db_partner = db.query(models.Partner).filter(models.Partner.id == partner_id).first()
-    return PartnerSerializer.serialize(db_partner)
+    result = query_partners(db).filter(models.Partner.id == partner_id).first()
+    
+    if result is None:
+        return None
+    
+    # Convert the result to a dictionary and parse 'coverageArea' and 'address' to GeoJSON
+    partner_dict = dict(result)
+    partner_dict["coverageArea"] = json.loads(partner_dict["coverageArea"])
+    partner_dict["address"] = json.loads(partner_dict["address"])
+
+    return partner_dict
 
 def get_partner_by_document(db: Session, document: str):
     db_partner = db.query(models.Partner).filter(models.Partner.document == document).first()
@@ -40,9 +51,24 @@ def create_partner(db: Session, partner: schemas.PartnerCreate):
     return PartnerSerializer.serialize(db_partner)
 
 def get_partners(db:Session, skip: int = 0, limit: int = 20):
-    db_partners = db.query(models.Partner).offset(skip).limit(limit).all()
+    db_partners = query_partners(db).offset(skip).limit(limit).all()
+
     results = []
     for db_partner in db_partners:
-        results.append(PartnerSerializer.serialize(db_partner))
+        # Convert the result to a dictionary and parse 'coverageArea' and 'address' to GeoJSON
+        partner_dict = dict(db_partner)
+        partner_dict["coverageArea"] = json.loads(partner_dict["coverageArea"])
+        partner_dict["address"] = json.loads(partner_dict["address"])
+        results.append(partner_dict)
 
     return results
+
+def query_partners(db:Session):
+    return db.query(
+        models.Partner.id,
+        models.Partner.tradingName,
+        models.Partner.ownerName,
+        models.Partner.document,
+        func.ST_AsGeoJSON(models.Partner.coverageArea).label('coverageArea'),
+        func.ST_AsGeoJSON(models.Partner.address).label('address')
+    )
--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def create_partner(partner: schemas.PartnerCreate, db: Session = Depends(get_db)
         raise HTTPException(status_code=400, detail="Document already registered")
     return crud.create_partner(db=db, partner=partner)
 
-@app.get("/partners/", response_model=list[schemas.PartnerResponse])
+@app.get("/partners/", response_model=list[schemas.Partner])
 def read_partners(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
     partners = crud.get_partners(db, skip=skip, limit=limit)
     return partners
