8a0d9764b867123e109ef61db971dc6712f4cc5d
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-06-29 04:20:33 +0330

    Add pydantic schema for partners

---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List
+
+class GeoJSONPoint(BaseModel):
+    type: str
+    coordinates: List[float]
+
+class GeoJSONMultiPolygon(BaseModel):
+    type: str
+    coordinates: List[List[List[List[float]]]]
+
+class PartnerBase(BaseModel):
+    id: str
+    tradingName: str
+    ownerName: str
+    document: str
+    coverageArea: GeoJSONMultiPolygon
+    address: GeoJSONPoint
+
+class PartnerCreate(PartnerBase):
+    pass
+
+class Partner(PartnerBase):
+    class Config:
+        orm_mode = True
