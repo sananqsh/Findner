6a0ed1ec28b2a96cc1880f32c31ee36fdccd5a78
Sanan Naghshbandi <s.naghshbandi@takhfifan.com>
2023-06-30 19:16:16 +0330

    Add more scenario to tests

--- tests/test_routes/test_partner.py
+++ tests/test_routes/test_partner.py
@@ -21,6 +21,7 @@ def test_create_partner():
         }
     }
 
+    # Success scenario
     response = client.post("/partners/", json=payload)
 
     assert response.status_code == 201
@@ -29,11 +30,29 @@ def test_create_partner():
     assert response.json()["document"] == "123456789/abcdef.something"
     assert response.json()["coverageArea"]["type"] == "MultiPolygon"
 
+    # Dupicate document scenario
+    response = client.post("/partners/", json=payload)
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Document already registered"
+
+    # Existing id scenario
+    payload["document"] += "new-document/1234.notDuplicate"
+    response = client.post("/partners/", json=payload)
+    assert response.status_code == 400
+    assert response.json()["detail"] == "id already exists"
+
+    # Resolved duplication scenario
+    payload["id"] = "11"
+    response = client.post("/partners/", json=payload)
+    assert response.status_code == 201
+
+
 def test_read_partners():
     response = client.get("/partners/")
 
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+
 
 def test_read_nearest_covering_partner():
     response = client.get("/partners/nearest?long=123.456&lat=12.345")
@@ -48,3 +67,30 @@ def test_read_nearest_covering_partner():
     assert response.json()["id"] == "1"
     assert response.json()["tradingName"] == "Test Partner"
 
+    # Creating a new Partner with same coverageArea, but closer in longitude
+    payload = {
+        "id": "2",
+        "tradingName": "Harry Somethinger",
+        "ownerName": "Ab Boss",
+        "document": "987/abc.something-new",
+        "coverageArea": {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [[[30, 20], [45, 40], [10, 40], [30, 20]]],
+                [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
+            ]
+        },
+        "address": {
+            "type": "Point",
+            "coordinates": [-20.57421, -21.785741]
+        }
+    }
+
+    response = client.post("/partners/", json=payload)
+
+    # Returns nearer Partner
+    response = client.get("/partners/nearest?long=30&lat=30")
+
+    assert response.status_code == 200
+    assert response.json()["id"] == "2"
+    assert response.json()["tradingName"] == "Harry Somethinger"
